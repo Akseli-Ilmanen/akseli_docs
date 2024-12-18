83bf605712addd6ea683a5f74725021a499ae57e
Akseli-Ilmanen <akseli.ilmanen@gmail.com>
2024-12-18 17:37:36 +0100

    fdajfklda

--- 01_update_pages.py
+++ 01_update_pages.py
@@ -2,16 +2,18 @@ import os
 import re
 import shutil
 
-# Set the path to the source directory containing images
 # Get the current working directory
 curr_directory = os.getcwd()
-print("Current Directory:", curr_directory)
+
+
+# Step 1: COPY IMAGES TO DESTINATION FOLDER AND EDIT MARKDOWN FILES
 
 # Navigate two levels up
-two_levels_up = os.path.abspath(os.path.join(curr_directory, "../../"))
+three_levels_up = os.path.abspath(os.path.join(curr_directory, "../../../"))
+print(f"Two levels up: {three_levels_up}")
 
-source_directory = os.path.join(two_levels_up, 'Z-Admin', 'image')
-
+# Get path to the source directory containing images
+source_directory = os.path.join(three_levels_up, 'Z-Admin', 'image')
 
 # Set the destination directory for copied images
 destination_directory = os.path.join(os.getcwd(), 'images')
@@ -22,14 +24,28 @@ os.makedirs(destination_directory, exist_ok=True)
 # Regular expression to match Markdown image references
 image_reference_pattern = r'!\[\[([^]]+)\]\]'
 
+# Get directory of markdown files to be copied
+os.chdir(os.path.join(three_levels_up, 'Projects', 'Copy to github pages'))
+print("Current Directory:", os.getcwd())
+
 # Get a list of all Markdown files in the current directory
 markdown_files = [f for f in os.listdir('.') if f.endswith('.md')]
+
+# Regular expressions for the replacements
+replacements = {
+    r'- !': '- ⚠️',
+    r'- no': '- ❌',
+    r'- \?': '- ❓',
+    r'- yes': '- ✅',
+    r'- &': '- 📚',
+    r'!\[\[([^]]+)\]\]': r'![image](images/\1)'
+}
 
 # Iterate through each Markdown file
 for md_file in markdown_files:
     with open(md_file, 'r', encoding='utf-8') as file:
         content = file.read()
-        
+                
         # Find all image references
         image_matches = re.findall(image_reference_pattern, content)
         
@@ -39,49 +55,16 @@ for md_file in markdown_files:
             if os.path.isfile(image_path):
                 # Copy the image to the destination folder
                 shutil.copy(image_path, destination_directory)
-
             else:
                 print(f"Image not found: {image_path}")
-    
-
-
-print("Image extraction complete.")
-
-
-# Set the source and destination directories
-source_directory2 = os.getcwd()
-destination_directory = os.path.join(source_directory2, 'md_changed')
-
-# Create the destination directory if it does not exist
-os.makedirs(destination_directory, exist_ok=True)
-
-# Regular expressions for the replacements
-replacements = {
-    r'- !': '- ⚠️',
-    r'- no': '- ❌',
-    r'- \?': '- ❓',
-    r'- yes': '- ✅',
-    r'- &': '- 📚',
-    r'!\[\[images/([^]]+)\]\]': r'![image](images/\1)'
-}
-
-# Get a list of all Markdown files in the source directory
-markdown_files = [f for f in os.listdir(source_directory2) if f.endswith('.md')]
-
-# Iterate through each Markdown file
-for md_file in markdown_files:
-    source_path = os.path.join(source_directory2, md_file)
-    destination_path = os.path.join(destination_directory, md_file)
-    
-    with open(source_path, 'r', encoding='utf-8') as file:
-        content = file.read()
         
         # Apply the replacements
         for pattern, replacement in replacements.items():
             content = re.sub(pattern, replacement, content)
     
     # Write the modified content to the destination file
+    destination_path = os.path.join(curr_directory, md_file)
     with open(destination_path, 'w', encoding='utf-8') as file:
         file.write(content)
 
-print("Markdown files have been processed and saved to the 'md_changed' folder.")
+print("Image extraction and markdown file processing complete.")
---
+++ index.md
@@ -0,0 +1,7 @@
+---
+title: Home page
+---
+
+Navigate to:
+- [Kilosort](2024-09-17 Kilosort4.md)
+
